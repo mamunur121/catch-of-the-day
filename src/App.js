8a616ac9d1cd0fcc35b325ee8edb49f12154b64f
Md mamunur Rashid <mamun.ruet.ete@gmail.com>
2020-01-16 21:50:52 +0100

    passing dynamic with data props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
-import StorePicker from "./components/StorePicker";
+import Header from "./components/Header";
+import Order from "./components/Order";
+import Inventory from "./components/Inventory";
 
 class App extends React.Component {
   render() {
     return (
-        <div>
-          <StorePicker />
+        <div className='catch-of-the-day'>
+          <div className='menu'>
+            <Header tagline='FRESH SEAFOOD MARKET'/>
+          </div>
+          <Order />
+          <Inventory />
         </div>
     );
   }
